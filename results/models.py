078b06a6816937bde38905c86b44bb6ace9e0cb9
aeron1020 <aeronpaduit20@gmail.com>
2025-10-15 21:08:25 +0800

    Initial commit - Civil Service Review App backend setup

---
+++ results/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from quizzes.models import Category
+
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    score = models.FloatField(default=0)
+    total_questions = models.IntegerField(default=0)
+    date_taken = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        user_display = self.user.username if self.user else "Anonymous"
+        return f"{user_display} - {self.category} ({self.score}%)"
