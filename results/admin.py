a5cef80afa0deac3e45e085a9c8b1813f202a70b
Your Name <you@example.com>
2025-10-17 13:49:09 +0800

    second commit

--- results/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Result
-
-@admin.register(Result)
-class ResultAdmin(admin.ModelAdmin):
-    list_display = ('user', 'category', 'score', 'total_questions', 'date_taken')
