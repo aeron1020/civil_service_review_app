8fd5cd21c18ed9eea1aefa764ba32c9d05470885
aeron1020 <aeronpaduit20@gmail.com>
2025-12-12 20:59:50 +0800

    cleaning comments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -185,7 +185,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
